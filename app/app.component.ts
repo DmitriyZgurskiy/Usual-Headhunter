bc35ed05afee2edcd08b1d43b56c48c815e37f5b
DmitriyZgurskiy <devDmitriyZgurskiy@gmail.com>
2017-01-20 10:06:50 +0200

    add Headhunter class and view him add styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,47 @@
 import { Component } from '@angular/core';
 
+export class Headhunter {
+  id: number;
+  name: string;
+  email: string;
+  skype: string;
+  linkedin: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hr.name}}</h2>
+    <div>
+      <label>id: </label>
+      {{hr.id}}
+    </div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hr.name" placeholder="name">
+    </div>
+    <div>
+      <label>email: </label>
+      <input [(ngModel)]="hr.email" placeholder="email">
+    </div>
+    <div>
+      <label>skype: </label>
+      <input [(ngModel)]="hr.skype" placeholder="skype">
+    </div>
+    <div>
+      <label>linkedin: </label>
+      <input [(ngModel)]="hr.linkedin" placeholder="linkedin">
+    </div>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  title = 'Usual-Headhunter';
+  hr: Headhunter = {
+    id: 1,
+    name: 'Sandra',
+    email: 'sandra@gmail.com',
+    skype: 'sandra',
+    linkedin: 'https://www.linkedin.com/sandra'
+  }
+}
